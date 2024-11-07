e1bbb476f13cdb638acffad7c3da681144260531
ingta <ing.talero@gmail.com>
2024-11-06 19:11:45 -0500

    Nuevo Repository

--- app.js
+++ app.js
@@ -57,5 +57,84 @@ function cargaAnimaciones(){
         });
     })
 }
+//Función para filtrar las categorias de los trabajos
+function verCategoria(cat){
+    const items =document.getElementsByClassName("item");
+    for(let i=0; i<items.length;i++){
+        items[i].style.display ="none";
+    }
 
-cargaAnimaciones();
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display ="block";
+    }
+
+    const links = document.querySelectorAll(".trabajos nav a");
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+
+    const itemSeleccionado = document.getElementsById(cat);
+    itemSeleccionado.className = "borde";
+}
+
+//Función que detecta cuando lanzar la animación de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("chart");
+        habilidades[0].classList.add("chart1");
+        habilidades[1].classList.add("chart2");
+        habilidades[2].classList.add("chart3");
+        habilidades[3].classList.add("chart4");
+        habilidades[4].classList.add("chart5");
+        habilidades[5].classList.add("chart6");
+
+        cargaAnimaciones();
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacióm de las habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//función que muestra el menu responsive
+
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className ===""){
+        x.className ="responsive";
+
+    //función que muestra el menu responsive    
+    let span = document.createElement("span");
+    document.getElementById("nav").appendChild(span);
+
+// quitamos el boton eliminar cuando se hace clik sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+
+}
+
+// Este codigo es para agregar una función a cada link del menu
+//responsive
+//cuando se presione en cualquier de los link del menu debe desaparecer el menu
+const links = document.querySelectorAll("#nav a");
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        //removemos el boton eliminar
+        btnEliminar = document.querySelector('#nav span');
+        btnEliminar.remove();
+    }
+}
